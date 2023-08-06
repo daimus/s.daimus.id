e7f5484a46a517e720dd93d1b12f976783c2c23a
Daimus Suudi <daimus.suudi@gmail.com>
2023-08-06 15:13:52 +0700

    refactor: change job title

--- src/components/Hero/Profile.tsx
+++ src/components/Hero/Profile.tsx
@@ -6,8 +6,8 @@ const Profile = () => {
             <div className="md:w-4/6 mx-auto text-center">
                 <Image src="/profile.jpg" width={120} height={120} alt="Muhammad Daimus Suudi"
                        className="h-32 w-32 rounded-full mx-auto mb-5"/>
-                <h1 className="font-bold">Muhammad Daimus Suudi</h1>
-                <p>Full Stack Developer</p>
+                <h1 className="font-bold">Daimus Suudi</h1>
+                <p>Software Engineer</p>
             </div>
         </>
     )
